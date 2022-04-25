fad950d511afa5230e6bd8076c18db67b839ad6c
Cristopher Rojas <103215941+estebanmp14@users.noreply.github.com>
2022-04-25 07:35:57 -0600

    Initial commit

---
+++ app/src/main/java/com/esteban/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.esteban.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        variablesYconstantes()
+    }
+
+    private fun variablesYconstantes() {
+        var myVariable = "hola maquinola"
+        println(myVariable)
+    }
+}
